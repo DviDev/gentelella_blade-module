7fe0a5e563b7d22d047beb961115844103219a9d
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-09 14:50:57 -0300

    initial files

---
+++ Database/Seeders/GentelellaDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Gentelella\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class GentelellaDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
