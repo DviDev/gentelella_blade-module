29dc470178032b62c2706f0d0233b77edc6a4bc7
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-10 01:37:19 -0300

    .

--- Routes/web.php
+++ Routes/web.php
@@ -17,4 +17,10 @@ Route::prefix('gentelella')->group(function () {
     Route::view('layout/v1', 'gentelella::components.layout.v1')->name('gentelella.layout.v1');
     Route::view('layout/v2', 'gentelella::components.layout.v2')->name('gentelella.layout.v2');
     Route::view('layout/v3', 'gentelella::components.layout.v3')->name('gentelella.layout.v3');
+    Route::view('example/form/general', 'gentelella::components.examples.form.generalformpage')->name('gentelella.example.form.general');
+    Route::view('example/form/advanced', 'gentelella::components.examples.form.formadvancedpage')->name('gentelella.example.form.advanced');
+    Route::view('example/form/validation', 'gentelella::components.examples.form.formvalidadionpage')->name('gentelella.example.form.validation');
+    Route::view('example/form/wizard', 'gentelella::components.examples.form.formwizardspage')->name('gentelella.example.form.wizard');
+    Route::view('example/form/upload', 'gentelella::components.examples.form.formuploadpage')->name('gentelella.example.form.upload');
+    Route::view('example/form/buttons', 'gentelella::components.examples.form.formbuttonspage')->name('gentelella.example.form.buttons');
 });
