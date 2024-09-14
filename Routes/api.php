5ebdda8fbb2fe2036838c64e1dedb7af5d54f72b
Davi Menezes <davimenezes.dev@gmail.com>
2024-09-14 17:51:22 -0300

    refactorings

--- Routes/api.php
+++ Routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
     |--------------------------------------------------------------------------
     | API Routes
@@ -14,6 +11,6 @@ use Illuminate\Support\Facades\Route;
     |
 */
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function () {
+/*Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function () {
     Route::get('gentelella', fn (Request $request) => $request->user())->name('gentelella');
-});
+});*/
