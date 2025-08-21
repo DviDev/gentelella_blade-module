41b34202c597b5e1413a40c43d90ded101bd2ef8
Davi Menezes <davimenezes.dev@gmail.com>
2025-08-21 13:14:36 -0300

    Refactor MenuItemEntityModel to ProjectModuleMenuItemEntityModel
    Replaced all occurrences of `MenuItemEntityModel` with `ProjectModuleMenuItemEntityModel` to improve clarity and modularity of menu item handling. This includes updates to namespaces, class names, traits, and method calls across various modules.

--- app/Listeners/CreateMenuItemsListener.php
+++ app/Listeners/CreateMenuItemsListener.php
@@ -6,7 +6,7 @@ use Modules\Permission\Enums\Actions;
 use Modules\Permission\Models\PermissionActionModel;
 use Modules\Person\Enums\UserType;
 use Modules\Project\Contracts\CreateMenuItemsListenerContract;
-use Modules\Project\Entities\MenuItem\MenuItemEntityModel;
+use Modules\Project\Entities\ProjectModuleMenuItem\ProjectModuleMenuItemEntityModel;
 use Modules\Project\Models\ProjectModuleEntityDBModel;
 use Modules\Project\Models\ProjectModuleMenuModel;
 
@@ -38,7 +38,7 @@ class CreateMenuItemsListener extends CreateMenuItemsListenerContract
 
     protected function createMenuItem(ProjectModuleMenuModel $menuModel, ?ProjectModuleEntityDBModel $entity = null, bool $active = true): void
     {
-        $p = MenuItemEntityModel::props();
+        $p = ProjectModuleMenuItemEntityModel::props();
         $menuModel->menuItems()->create([
             $p->label => __('Page Examples'),
             $p->num_order => 1,
