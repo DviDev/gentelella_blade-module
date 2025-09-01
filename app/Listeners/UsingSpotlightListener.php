dc2616d1150e9ffd1dc717a764d6dfaf90489393
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-01 17:57:51 -0300

    Remove unused constructor methods from event listeners
    Removed empty constructor methods across multiple event listener classes to improve code clarity and reduce redundancy. This change simplifies the structure of the listeners without affecting their functionality.

--- app/Listeners/UsingSpotlightListener.php
+++ app/Listeners/UsingSpotlightListener.php
@@ -6,17 +6,7 @@ use Modules\Base\Events\UsingSpotlightEvent;
 
 class UsingSpotlightListener
 {
-    /**
-     * Create the event listener.
-     */
-    public function __construct()
-    {
-        //
-    }
 
-    /**
-     * Handle the event.
-     */
     public function handle(UsingSpotlightEvent $event): void
     {
         if (str($event->uri)->startsWith(['gentelella', '/gentelella'])) {
