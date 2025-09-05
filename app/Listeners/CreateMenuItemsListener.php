0977632fc8b197cc4fa93db546df8b80bb238452
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-05 12:57:37 -0300

    Refactor CreateMenuItemsListener to simplify menu item creation logic
    Streamlined the `CreateMenuItemsListener` class by removing redundant methods and combining menu creation logic. Adjusted translation calls for consistency and improved code readability.

--- app/Listeners/CreateMenuItemsListener.php
+++ app/Listeners/CreateMenuItemsListener.php
@@ -12,28 +12,9 @@ use Modules\Project\Models\ProjectModuleMenuModel;
 
 class CreateMenuItemsListener extends CreateMenuItemsListenerContract
 {
-    public function handle($event): void
-    {
-        if (ProjectModuleMenuModel::query()->where('name', $this->moduleName())->exists()) {
-            return;
-        }
-
-        $menu = $this->createMenu($this->moduleName(), $this->moduleName(), 1);
-
-        $this->createMenuItem($menu);
-    }
-
     public function moduleName(): string
     {
         return 'Gentelella';
-    }
-
-    protected function createMenu($name, $title, $order = 1): ProjectModuleMenuModel
-    {
-        return ProjectModuleMenuModel::firstOrCreate(
-            ['name' => $name],
-            ['title' => $title, 'num_order' => $order, 'active' => true]
-        );
     }
 
     protected function createMenuItem(ProjectModuleMenuModel $menuModel, ?ProjectModuleEntityDBModel $entity = null, bool $active = true): void
@@ -42,7 +23,7 @@ class CreateMenuItemsListener extends CreateMenuItemsListenerContract
         $menuModel->menuItems()->create([
             $p->label => __('Page Examples'),
             $p->num_order => 1,
-            $p->title => trans('Page Examples'),
+            $p->title => __('Page Examples'),
             $p->icon => '<i class="nav-icon fas fa-circle fa-xs text-xs"></i>',
             $p->url => route('gentelella.layout.v1'),
             $p->active => true,
@@ -52,10 +33,12 @@ class CreateMenuItemsListener extends CreateMenuItemsListenerContract
 
     protected function getAction(): PermissionActionModel
     {
-        $action = PermissionActionModel::query()
-            ->create(['name' => Actions::view->name, 'title' => __('Page Examples')]);
-        $action->firstOrCreateGroup()
-            ->createCondition(UserType::DEVELOPER);
+        $action = PermissionActionModel::query()->create([
+            'name' => Actions::view->name,
+            'title' => __('Page Examples'),
+        ]);
+
+        $action->firstOrCreateGroup()->createCondition(UserType::DEVELOPER);
 
         return $action;
     }
