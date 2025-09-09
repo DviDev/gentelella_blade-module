4b317cc59650501cbcc9196b35e10f2e0a0936a5
Davi Menezes <davimenezes.dev@gmail.com>
2025-09-09 03:39:10 -0300

    Register `ScanTableGentelellaListener` for handling `ScanTableEvent` in `GentelellaEventServiceProvider`.

--- app/Providers/GentelellaEventServiceProvider.php
+++ app/Providers/GentelellaEventServiceProvider.php
@@ -4,7 +4,9 @@ namespace Modules\Gentelella\Providers;
 
 use Event;
 use Illuminate\Support\ServiceProvider;
+use Modules\DBMap\Events\ScanTableEvent;
 use Modules\Gentelella\Listeners\CreateMenuItemsListener;
+use Modules\Gentelella\Listeners\ScanTableGentelellaListener;
 use Modules\Project\Events\CreateMenuItemsEvent;
 
 class GentelellaEventServiceProvider extends ServiceProvider
@@ -12,5 +14,6 @@ class GentelellaEventServiceProvider extends ServiceProvider
     public function register()
     {
         Event::listen(CreateMenuItemsEvent::class, CreateMenuItemsListener::class);
+        Event::listen(ScanTableEvent::class, ScanTableGentelellaListener::class);
     }
 }
